61b36293d5a2d612b4b3dfd5fcbeaf56a0390ba9
Your Name <you@example.com>
2020-09-12 22:33:16 +0300

    first commit

---
+++ PAD_1/src/main/java/ClientServer/Broker.java
@@ -0,0 +1,29 @@
+package ClientServer;
+
+import OtherClasses.BrokerService;
+import OtherClasses.IOperation;
+import java.io.IOException;
+
+public class Broker {
+    public static void main(String[] args) throws IOException {
+        System.out.println("BROKER OPTIONS:");
+        IOperation broker=new BrokerService();
+        String msg;
+        while(true)
+        {
+            while(!(msg=broker.readAsync()).isEmpty())
+                if(msg.equals("invalid"))
+                {
+                    System.out.println("--Broker loop--");
+                    System.out.println("--INVALID MESSAGE--");
+                }
+                else
+                {
+                    System.out.println("--Broker loop--");
+                    System.out.println("--VALID MESSAGE--");
+                    broker.writeAsync(msg);
+                }
+        }
+    }
+}
+
